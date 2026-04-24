b3506a4e25e27b1cd7882c081dc15c1b9b8a8b86
Xopher <x@hallofthegods.com>
2026-04-23 23:05:09 -0700

    refactor: update and rotate production build assets in the public distribution directory

--- public/dist/js/useSparkNav-uLiG244W.js
+++
@@ -1,1 +0,0 @@
-import{i as s}from"./youmeos-BX2918Hk.js";import{e as m}from"./vendor-C1_nKTTn.js";const U=r=>{const t=s(),n=m(()=>t.windows[r]?.urme||[]);return{currentUrme:n,navigate:(...e)=>{const o=Array.isArray(e[0])?e[0]:e;t.setSparkUrme(r,o)},pushUrme:e=>{const o=[...n.value];o.push(e),t.setSparkUrme(r,o)},popUrme:()=>{const e=[...n.value];e.pop(),t.setSparkUrme(r,e)}}};export{U as u};
