ac58b5ca4b2549ecaa86cfd4621fbe691b5fa51d
Xopher <x@hallofthegods.com>
2026-08-20 23:13:40 -0700

    chore: update build artifacts and manifest by rotating application assets

--- public/dist/js/useSparkNav-D55ueTjz.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./youmeos-C7LfiD6b.js";import{e as m}from"./vendor-B-4gaiIj.js";const i=r=>{const t=s(),n=m(()=>t.windows[r]?.urme||[]);return{currentUrme:n,navigate:(...e)=>{const o=Array.isArray(e[0])?e[0]:e;t.setSparkUrme(r,o)},pushUrme:e=>{const o=[...n.value];o.push(e),t.setSparkUrme(r,o)},popUrme:()=>{const e=[...n.value];e.pop(),t.setSparkUrme(r,e)}}};export{i as u};
