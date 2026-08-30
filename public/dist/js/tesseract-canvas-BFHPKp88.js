bdb297260eeeec0b3e6a80b0cee1b565ded52ae5
Xopher <x@hallofthegods.com>
2026-08-29 21:10:40 -0700

    build: rebuild frontend assets and update Vite manifest with new component chunks

---
+++ public/dist/js/tesseract-canvas-BFHPKp88.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./tesseract-canvas.vue_vue_type_script_setup_true_lang-3NucCT0u.js";import"./vendor-2n2T-r6g.js";import"./vendor-core-BCGDVfqk.js";export{o as default};
--- public/dist/js/tesseract-canvas-i4x8ZXED.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./tesseract-canvas.vue_vue_type_script_setup_true_lang-D9aVSbCD.js";import"./vendor-j-5sGUGO.js";import"./vendor-core-BCGDVfqk.js";export{o as default};
