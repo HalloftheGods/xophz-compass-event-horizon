40cf92213840945a9cde9c8ba58ffd0536c06f8c
XP <x@hallofthegods.com>
2026-09-07 09:56:51 -0700

    refactor: rebuild and refresh dist assets for plugin apps and views

--- public/dist/js/tesseract-canvas-CEQdP7fb.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./tesseract-canvas.vue_vue_type_script_setup_true_lang-Dr8iR1eS.js";import"./vendor-OkeWRhMK.js";import"./vendor-core-BCGDVfqk.js";export{o as default};
---
+++ public/dist/js/tesseract-canvas-CrMb0rTw.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./tesseract-canvas.vue_vue_type_script_setup_true_lang-BCT3RLyk.js";import"./vendor-DJDm7qH9.js";import"./vendor-core-BCGDVfqk.js";export{o as default};
